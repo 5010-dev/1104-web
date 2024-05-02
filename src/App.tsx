8babcc02c749f711d76fc70360ada3d0bf992e22
ED <granrr.z@gmail.com>
2024-05-02 16:32:25 +0900

    Routing initialised & Navigation component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/home.page'
+import Products from './pages/products.page'
+
 import './App.css'
 
 function App() {
-	return <div className="App"></div>
+	return (
+		<Router>
+			<div className="App">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/products" element={<Products />} />
+				</Routes>
+			</div>
+		</Router>
+	)
 }
 
 export default App
---
+++ src/components/navigation.components.tsx/navigation.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const NavigationContainer = styled.header`
+	width: 100%;
+
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+
+	padding: 1.5rem;
+
+	background-color: rgba(255, 255, 255, 0.25); // this for testing
+`
