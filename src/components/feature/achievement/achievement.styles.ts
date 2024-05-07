69074838d3efe5393b164d489861ae861f784df6
ED <granrr.z@gmail.com>
2024-05-08 01:05:50 +0900

    Achievement component & styles adjusted

--- src/components/feature/achievement/achievement.styles.ts
+++ src/components/feature/achievement/achievement.styles.ts
@@ -1,4 +1,5 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
+
 import { SectionContainer } from '../../global/section/section.styles'
 import { getTypography } from '../../../utils/typo.utils'
 import { getDeviceTypePadding } from '../../../utils/device.utils'
