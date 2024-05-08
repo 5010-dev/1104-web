fff97e91c6b16437dfdc1e792e5a164f9f8814c7
ED <granrr.z@gmail.com>
2024-05-09 00:41:10 +0900

    Hero component styled modified

--- src/hooks/useSafariDetection.tsx
+++ src/hooks/useSafariDetection.tsx
@@ -1,3 +1,31 @@
+// import { useEffect, useState } from 'react'
+
+// const useSafariDetection = (): {
+// 	isSafariMobile: boolean
+// 	isSafariTablet: boolean
+// } => {
+// 	const [isSafariMobile, setIsSafariMobile] = useState(false)
+// 	const [isSafariTablet, setIsSafariTablet] = useState(false)
+
+// 	useEffect(() => {
+// 		const userAgent = navigator.userAgent.toLowerCase()
+// 		setIsSafariMobile(
+// 			/safari/.test(userAgent) &&
+// 				/mobile/.test(userAgent) &&
+// 				!/chrome/.test(userAgent),
+// 		)
+// 		setIsSafariTablet(
+// 			/safari/.test(userAgent) &&
+// 				/tablet/.test(userAgent) &&
+// 				!/chrome/.test(userAgent),
+// 		)
+// 	}, [])
+
+// 	return { isSafariMobile, isSafariTablet }
+// }
+
+// export default useSafariDetection
+
 import { useEffect, useState } from 'react'
 
 const useSafariDetection = (): {
@@ -9,16 +37,14 @@ const useSafariDetection = (): {
 
 	useEffect(() => {
 		const userAgent = navigator.userAgent.toLowerCase()
-		setIsSafariMobile(
-			/safari/.test(userAgent) &&
-				/mobile/.test(userAgent) &&
-				!/chrome/.test(userAgent),
-		)
-		setIsSafariTablet(
-			/safari/.test(userAgent) &&
-				/tablet/.test(userAgent) &&
-				!/chrome/.test(userAgent),
-		)
+		const platform = navigator.platform
+
+		const isSafari = /safari/.test(userAgent) && !/chrome/.test(userAgent)
+		const isIOS =
+			platform === 'iPad' || platform === 'iPhone' || platform === 'iPod'
+
+		setIsSafariMobile(isSafari && isIOS && /mobile/.test(userAgent))
+		setIsSafariTablet(isSafari && isIOS && /tablet/.test(userAgent))
 	}, [])
 
 	return { isSafariMobile, isSafariTablet }
