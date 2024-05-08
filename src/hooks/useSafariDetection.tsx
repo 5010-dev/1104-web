651b4bfe87f9ab615bc907255929284240476470
ED <granrr.z@gmail.com>
2024-05-09 02:33:51 +0900

    Hero component styled modified for css value problem

--- src/hooks/useSafariDetection.tsx
+++ src/hooks/useSafariDetection.tsx
@@ -31,23 +31,39 @@ import { useEffect, useState } from 'react'
 const useSafariDetection = (): {
 	isSafariMobile: boolean
 	isSafariTablet: boolean
+	isSafariDesktop: boolean
 } => {
 	const [isSafariMobile, setIsSafariMobile] = useState(false)
 	const [isSafariTablet, setIsSafariTablet] = useState(false)
+	const [isSafariDesktop, setIsSafariDesktop] = useState(false)
 
 	useEffect(() => {
 		const userAgent = navigator.userAgent.toLowerCase()
 		const isSafari = /safari/.test(userAgent) && !/chrome/.test(userAgent)
 		const isIOS = /ipad|iphone|ipod/.test(userAgent)
-		const isIPad = /ipad/.test(userAgent)
+		const isMacintosh = /macintosh/.test(userAgent)
 		const isTouchSupported =
 			'ontouchstart' in window || navigator.maxTouchPoints > 0
 
-		setIsSafariMobile(isSafari && isIOS && !isIPad)
-		setIsSafariTablet(isSafari && isIOS && isIPad && isTouchSupported)
+		const isMobileScreen = window.innerWidth < 768
+		const isIPadProScreen =
+			window.innerWidth >= 1024 && window.devicePixelRatio >= 2
+
+		setIsSafariMobile(isSafari && isIOS && !isMacintosh && isMobileScreen)
+		setIsSafariTablet(
+			isSafari &&
+				isIOS &&
+				isMacintosh &&
+				isTouchSupported &&
+				!isMobileScreen &&
+				!isIPadProScreen,
+		)
+		setIsSafariDesktop(
+			isSafari && isMacintosh && !isTouchSupported && !isIPadProScreen,
+		)
 	}, [])
 
-	return { isSafariMobile, isSafariTablet }
+	return { isSafariMobile, isSafariTablet, isSafariDesktop }
 }
 
 export default useSafariDetection
