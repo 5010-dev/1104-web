c2210e9fd52597f8e38e24f772c35e8de590013c
ED <granrr.z@gmail.com>
2024-05-17 09:18:57 +0900

    Account component modified

--- src/pages/feature/account/account.page.tsx
+++ src/pages/feature/account/account.page.tsx
@@ -6,6 +6,9 @@ import { useAuthDataStore } from '../../../store/authDataStore'
 
 import { AccountContainer } from './account.styles'
 
+import { ReactComponent as TradingviewLogo } from '../../../assets/logo/tradingview-logo.svg'
+import { ReactComponent as DiscordLogo } from '../../../assets/logo/discord-logo.svg'
+
 export default function Account() {
 	const deviceType = useDeviceTypeStore((state) => state.deviceType)
 	const { userId } = useAuthDataStore((state) => state.loginUser)
@@ -21,7 +24,29 @@ export default function Account() {
 		<AccountContainer $deviceType={deviceType}>
 			<div id="contents-container">
 				<h1 id="heading">내 정보</h1>
-				{userId}
+				<div id="account-info-container">
+					<div className="container-row">
+						<h2 className="heading-2">아이디</h2>
+						<p className="body">{userId}</p>
+					</div>
+					<hr />
+					<div className="container-row">
+						<h2 className="heading-2">연결된 계정</h2>
+						<span>
+							<TradingviewLogo className="logo" />
+							트레이딩뷰
+						</span>
+						<span>
+							<DiscordLogo className="logo" />
+							디스코드
+						</span>
+					</div>
+					<hr />
+					<div className="container-row">
+						<h2 className="heading-2">구독 정보</h2>
+						<p className="body">구독중인 서비스가 없습니다.</p>
+					</div>
+				</div>
 			</div>
 		</AccountContainer>
 	)
--- src/pages/feature/account/account.styles.ts
+++ src/pages/feature/account/account.styles.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 import { getDeviceTypePadding } from '../../../utils/device.utils'
 import { getTypography } from '../../../utils/typo.utils'
-import { getColour } from '../../../utils/colour.utils'
+import { getColour, hexToRgba } from '../../../utils/colour.utils'
+import { getContainerStyle } from '../../../utils/style.utils'
 
 import PageLayoutContainer from '../../global/page-layout/page-layout.styles'
 
@@ -29,6 +30,72 @@ export const AccountContainer = styled(PageLayoutContainer)`
 			${({ theme }) => getTypography(theme, 'heading1')}
 			color: ${({ theme }) =>
 				getColour(theme, 'neutral', 'secondary', 'active')};
+		}
+
+		div#account-info-container {
+			width: ${({ theme }) => theme.layout.container.width};
+
+			${({ theme }) =>
+				getContainerStyle(
+					theme,
+					'neutral',
+					'secondary',
+					'filled',
+					'rounded2',
+					0.05,
+				)}
+
+			display: flex;
+			flex-direction: column;
+			justify-content: flex-start;
+			align-items: flex-start;
+			gap: ${({ theme }) => theme.layout.section.gutter};
+
+			padding: ${({ theme }) =>
+				`${theme.layout.section.gutter} ${theme.layout.container.padding.default}`};
+
+			div.container-row {
+				width: ${({ theme }) => theme.layout.container.width};
+
+				display: flex;
+				flex-direction: column;
+				justify-content: flex-start;
+				align-items: flex-start;
+				gap: ${({ theme }) => theme.layout.component.gutter};
+
+				h2.heading-2 {
+					${({ theme }) => getTypography(theme, 'body')}
+
+					color: ${({ theme }) =>
+						getColour(theme, 'neutral', 'secondary', 'inactive')};
+				}
+
+				p.body {
+					${({ theme }) => getTypography(theme, 'subheading')}
+					color: ${({ theme }) =>
+						getColour(theme, 'neutral', 'secondary', 'active')};
+				}
+
+				span {
+					display: flex;
+					justify-content: flex-start;
+					align-items: center;
+					gap: 0.5rem;
+
+					.logo {
+						height: 1.5rem;
+						width: 1.8rem;
+					}
+				}
+			}
+
+			hr {
+				width: 100%;
+				height: 0.0625rem;
+				border: none;
+				background-color: ${({ theme }) =>
+					hexToRgba(getColour(theme, 'neutral', 'secondary', 'active'), 0.1)};
+			}
 		}
 	}
 `
