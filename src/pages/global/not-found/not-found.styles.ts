76107295e5aeb16a353ef5c98758b39a7c019682
ED <granrr.z@gmail.com>
2024-05-21 15:41:39 +0900

    NotFound page styles modified

--- src/pages/global/not-found/not-found.styles.ts
+++ src/pages/global/not-found/not-found.styles.ts
@@ -30,6 +30,12 @@ export const NotFoundContainer = styled.div<NotFoundContainerProps>`
 	background-position: center;
 	background-repeat: no-repeat;
 
+	-webkit-filter: grayscale(100%);
+	-moz-filter: grayscale(100%);
+	-ms-filter: grayscale(100%);
+	-o-filter: grayscale(100%);
+	filter: grayscale(100%);
+
 	&::before {
 		content: '';
 		position: absolute;
