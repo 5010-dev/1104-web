7f3e3d17c41301b82e5e9cc6f12f6aaa6b44bfe8
ED <granrr.z@gmail.com>
2024-06-11 17:46:31 +0900

    Token utils changed

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,5 +2,6 @@
 declare namespace NodeJS {
 	interface ProcessEnv {
 		REACT_APP_BASE_URL: string
+		REACT_APP_SECRET_KEY: string
 	}
 }
--- src/utils/token.utils.ts
+++ src/utils/token.utils.ts
@@ -1,4 +1,8 @@
 import { useAuthDataStore } from '../store/authDataStore'
+import CryptoJS from 'crypto-js'
+
+const REFRESH_TOKEN_KEY = 'refresh'
+const SECRET_KEY = process.env.REACT_APP_SECRET_KEY
 
 export const setAccessToken = (token: string) => {
 	useAuthDataStore.getState().updateLoginUser('accessToken', token)
@@ -12,24 +16,20 @@ export const removeAccessToken = () => {
 	useAuthDataStore.getState().updateLoginUser('accessToken', '')
 }
 
+export const setRefreshToken = (token: string) => {
+	const encryptedToken = CryptoJS.AES.encrypt(token, SECRET_KEY).toString()
+	localStorage.setItem(REFRESH_TOKEN_KEY, encryptedToken)
+}
+
 export const getRefreshToken = () => {
-	const cookies = document.cookie.split('; ')
-	const refreshTokenCookie = cookies.find((cookie) =>
-		cookie.startsWith('refreshToken'),
-	)
-	if (refreshTokenCookie) {
-		return refreshTokenCookie.split('=')[1]
+	const encryptedToken = localStorage.getItem(REFRESH_TOKEN_KEY)
+	if (encryptedToken) {
+		const bytes = CryptoJS.AES.decrypt(encryptedToken, SECRET_KEY)
+		return bytes.toString(CryptoJS.enc.Utf8)
 	}
 	return null
 }
 
-// export const setRefreshToken = (token: string) => {
-// 	document.cookie = `refreshToken=${token}; HttpOnly; Secure; SameSite=Lax; path=/; Max-Age=${
-// 		24 * 60 * 60
-// 	}`
-// }
-
-// export const removeRefreshToken = (token: string) => {
-// 	document.cookie =
-// 		'refreshToken=; HttpOnly; Secure; SameSite=Lax; path=/; Max-Age=0'
-// }
+export const removeRefreshToken = () => {
+	localStorage.removeItem(REFRESH_TOKEN_KEY)
+}
