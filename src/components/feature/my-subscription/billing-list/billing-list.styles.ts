1f520055b6a4f4e7f80a02627df56cea2516fb06
weirdry <granrr.z@gmail.com>
2024-07-10 21:19:52 +0900

    BillingList styles modifief

--- src/components/feature/my-subscription/billing-list/billing-list.styles.ts
+++ src/components/feature/my-subscription/billing-list/billing-list.styles.ts
@@ -1,10 +1,20 @@
 import styled from 'styled-components'
 
+import { getTypography } from '../../../../utils/typo.utils'
+import { getColour } from '../../../../utils/colour.utils'
+
 export const BillingListContainer = styled.div`
 	width: ${({ theme }) => theme.layout.container.width};
 
 	display: flex;
 	flex-direction: column;
 	justify-content: flex-start;
-	align-items: center;
+	align-items: flex-start;
+
+	p#billing-list-body {
+		${({ theme }) => getTypography(theme, 'body')}
+		color: ${({ theme }) => getColour(theme, 'neutral', 'secondary', 'active')};
+
+		text-align: left;
+	}
 `
